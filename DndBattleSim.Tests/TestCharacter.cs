4c36e3cb3b740c10841da120cf3796e9141f5dcb
jasonxanthakis <iasonas.xanthakis@gmail.com>
2025-08-14 11:23:02 +0100

    Successfully tested all shared character functionality

--- DndBattleSim.Tests/TestCharacter.cs
+++ DndBattleSim.Tests/TestCharacter.cs
@@ -26,7 +26,7 @@ namespace DndBattleSim.Tests
             this.mockRandomiser2.Setup(rnd => rnd.Next(1, 11)).Returns(10);
         }
 
-        [TestCase]
+        [Test]
         public void Check_That_HP_Value_Is_Expected_Value()
         {
             // Act
@@ -38,7 +38,7 @@ namespace DndBattleSim.Tests
             Assert.That(character2.HP, Is.EqualTo(10));
         }
 
-        [TestCase]
+        [Test]
         public void Check_That_Attack_Value_Is_Expected_Value()
         {
             // Act
@@ -48,6 +48,17 @@ namespace DndBattleSim.Tests
             // Assert
             Assert.That(character1.Attack, Is.EqualTo(1));
             Assert.That(character2.Attack, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void Check_That_GotHit_Acts_As_Intended()
+        {
+            // Act
+            var character = new TestCharacterStub(this.mockRandomiser2.Object);
+            character.GotHit(5);
+
+            // Assert
+            Assert.That(character.HP, Is.EqualTo(5));
         }
     }
 }
